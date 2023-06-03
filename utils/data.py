915235927ea2a4302ac70fd9c765bcc6c2552b79
github suin00h <suin00h@korea.ac.kr>
2023-06-04 03:16:58 +0900

    Update: EP stage
    EP model design,
    util files update,
    PE model notebook update,
    state and loss update

--- utils/data.py
+++ utils/data.py
@@ -1,11 +1,13 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence as _pad
 
 __all__ = [
-    '_collate',
+    'collate_PE',
+    'collate_EP',
 ]
 
-# custom collate function for variable-size input
-def _collate(batch):
+# custom collate function for variable-size input training PE model
+def collate_PE(batch):
     return {
         'image': [item['image'] for item in batch],                         # list of tensors [B]: (tensor)[3, H, W]
         'seq': torch.tensor([item['seq'] for item in batch]),               # [B]
@@ -15,3 +17,17 @@ def _collate(batch):
         'bbox': torch.stack([item['bbox'] for item in batch]),              # [B, 4]
         'keypoints': torch.stack([item['keypoints'] for item in batch]),    # [B, 15, 3]
     }
+
+# custom collate function for variable-size input training EP model
+def collate_EP(batch):
+    keys = _pad(
+        sequences=[item[0] for item in batch],
+        batch_first=True,
+        padding_value=-1)
+    action = torch.stack([item[1] for item in batch]).squeeze()
+    emotion = torch.stack([item[2] for item in batch]).squeeze()
+    
+    return {
+        'keys': keys,
+        'action': action,
+        'emotion': emotion}
