79c7cbf02a860a0b62d8c3921379013319fca1ac
github suin00h <suin00h@korea.ac.kr>
2023-06-04 22:22:00 +0900

    Update: 4th training
    Efficiently removed burden of dataloader

--- utils/data.py
+++ utils/data.py
@@ -1,9 +1,12 @@
 import torch
+import ast
 from torch.nn.utils.rnn import pad_sequence as _pad
 
 __all__ = [
     'collate_PE',
     'collate_EP',
+    'to_tensor',
+    'norm_keys',
 ]
 
 # custom collate function for variable-size input training PE model
@@ -31,3 +34,19 @@ def collate_EP(batch):
         'keys': keys,
         'action': action,
         'emotion': emotion}
+
+def to_tensor(series):
+    return torch.tensor(series.apply(ast.literal_eval).values.tolist(), dtype=float)
+
+def norm_keys(bbox, keys):
+    for i in range(bbox.shape[0]):
+        w = bbox[i][2] - bbox[i][0]
+        h = bbox[i][3] - bbox[i][1]
+
+        keys[i, :, 0] = (keys[i, :, 0] - bbox[i][0]) / w
+        keys[i, :, 1] = (keys[i, :, 1] - bbox[i][1]) / h
+
+        for j in range(15):
+            if keys[i, j, -1] == 0:
+                keys[i, j] = torch.tensor([0, 0, 0], dtype=float)
+    return keys
